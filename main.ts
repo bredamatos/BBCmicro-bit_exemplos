41a67293e3ddbac0a2f936c94021b215742fc4f0
bredamatos <105065939+bredamatos@users.noreply.github.com>
2023-03-08 11:18:47 +0000

    On/Off LED com botão externo.

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+let LED = 0
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        if (LED) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            LED = 0
+            basic.pause(500)
+        } else {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            LED = 1
+            basic.pause(500)
+        }
+    }
+})
